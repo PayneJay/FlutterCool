38c8027689d47de25651a805b2fd51e2d656f714
weipengjie <pengjiew@tujia.com>
2019-05-28 18:22:02 +0800

    添加图片选择库，解决AndroidX的问题

--- lib/business/profile/ProfileScreen.dart
+++ lib/business/profile/ProfileScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/widget/EmptyPage.dart';
+import 'package:myapp/widget/RatingPage.dart';
+import 'package:myapp/business/profile/UserEditPage.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -9,8 +10,99 @@ class ProfileScreen extends StatefulWidget {
 class ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: new EmptyPage(),
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              children: <Widget>[
+                new GestureDetector(
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage(
+                      "https://p1.ssl.qhimgs1.com/bdr/576__/t0192dd8448f034a631.jpg",
+                    ),
+                  ),
+                  onTap: () {
+                    _selectAvatar();
+                  },
+                ),
+                new Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: new Text(
+                    'FlutterCool',
+                    style: new TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: items
+                .map((item) => Column(
+                      children: <Widget>[
+                        GestureDetector(
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                height: kTextTabBarHeight,
+                                alignment: Alignment.center,
+                                padding:
+                                    const EdgeInsets.only(left: 15, right: 15),
+                                child: Text(
+                                  item.title,
+                                  style: TextStyle(fontSize: 16),
+                                ),
+                              )
+                            ],
+                          ),
+                          onTap: () {
+                            _onItemClick(item.id);
+                          },
+                        ),
+                        Divider(
+                          height: 0,
+                          color: Colors.black12,
+                        )
+                      ],
+                    ))
+                .toList(),
+          )
+        ],
+      ),
     );
   }
+
+  _onItemClick(int id) {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new RatingPage();
+    }));
+  }
+
+  void _selectAvatar() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      return new UserEditPage();
+    }));
+  }
 }
+
+class ListItem {
+  const ListItem({this.title, this.id});
+
+  final String title;
+  final int id;
+}
+
+const List<ListItem> items = const <ListItem>[
+  const ListItem(title: '我的待读', id: 1),
+  const ListItem(title: '我的收藏', id: 2),
+  const ListItem(title: '我的推刊', id: 3),
+  const ListItem(title: '消息通知', id: 4),
+  const ListItem(title: '阅读历史', id: 5),
+  const ListItem(title: '夜间模式', id: 6),
+  const ListItem(title: '鼓励一顿', id: 7),
+  const ListItem(title: '离线阅读', id: 8),
+  const ListItem(title: '检查更新', id: 9),
+  const ListItem(title: '意见反馈', id: 10),
+  const ListItem(title: '关于我们', id: 11),
+];
--- lib/widget/RatingPage.dart
+++ lib/widget/RatingPage.dart
@@ -1,72 +1,64 @@
 import 'package:flutter/material.dart';
-import "package:pull_to_refresh/pull_to_refresh.dart";
 
-class RatingPage extends StatefulWidget {
-  @override
-  PullToRefresh createState() => new PullToRefresh();
-}
-
-class PullToRefresh extends State<RatingPage> {
-//  RefreshMode  refreshing = RefreshMode.idle;
-//  LoadMode loading = LoadMode.idle;
-  RefreshController _refreshController;
-  List<Widget> data = [];
-
-  void _getDatas() {
-    for (int i = 0; i < 14; i++) {
-      data.add(new Card(
-        margin:
-        new EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
-        child: new Center(
-          child: new Text('Data $i'),
-        ),
-      ));
-    }
-  }
-
-  void enterRefresh() {
-    _refreshController.requestRefresh();
-  }
-
-  @override
-  void initState() {
-    _getDatas();
-    _refreshController = new RefreshController();
-    super.initState();
-  }
-
+class RatingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('PullToRefresh'),
+    //因为本路由没有使用Scaffold，为了让子级Widget(如Text)使用
+    //Material Design 默认的样式风格,我们使用Material作为本路由的根。
+    return Material(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          //AppBar，包含一个导航栏
+          SliverAppBar(
+            pinned: true,
+            expandedHeight: 250.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('Minerva'),
+              background: Image.asset(
+                "./images/img_default.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(8.0),
+            sliver: new SliverGrid(
+              //Grid
+              gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, //Grid按两列显示
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 4.0,
+              ),
+              delegate: new SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  //创建子widget
+                  return new Container(
+                    alignment: Alignment.center,
+                    color: Colors.cyan[100 * (index % 9)],
+                    child: new Text('grid item $index'),
+                  );
+                },
+                childCount: 20,
+              ),
+            ),
+          ),
+          //List
+          new SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: new SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  //创建列表项
+                  return new Container(
+                    alignment: Alignment.center,
+                    color: Colors.lightBlue[100 * (index % 9)],
+                    child: new Text('list item $index'),
+                  );
+                }, childCount: 50 //50个列表项
+            ),
+          ),
+        ],
       ),
-      body: new Container(
-          child: new Text("content")),
     );
-  }
-}
-
-class Item extends StatefulWidget {
-  @override
-  _ItemState createState() => new _ItemState();
-}
-
-class _ItemState extends State<Item> {
-  @override
-  Widget build(BuildContext context) {
-    return new Card(
-      margin:
-      new EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
-      child: new Center(
-        child: new Text('Data'),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    print("销毁");
-    super.dispose();
   }
 }
